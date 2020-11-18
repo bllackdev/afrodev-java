d581be82ba2aa068d61cb0eb2617f66a944a54b8
bllackdev <rapha_rmm@hotmail.com>
2020-11-17 22:37:24 -0300

    Exemplo do Greeting

--- exerc-aula13/exemplo-spring/src/main/java/com/afrodev/exemplospring/ExemploSpringApplication.java
+++ exerc-aula13/exemplo-spring/src/main/java/com/afrodev/exemplospring/ExemploSpringApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @SpringBootApplication
 @RestController
 public class ExemploSpringApplication {
@@ -29,6 +31,14 @@ public class ExemploSpringApplication {
 	public String subtracao(@RequestParam(value = "valor", defaultValue = "0") Double valor,
 					   @RequestParam(value = "valor2", defaultValue = "0") Double valor2) {
 		return String.format("Soma dos numeros: %s + %s = %s", valor, valor2, (valor - valor2));
+	}
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Spring Boot") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
 
